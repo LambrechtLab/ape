5973943a5ce932062f4b5c57835df60509ad7c26
meb168 <meb168@login0b.frank.sam.pitt.edu>
2015-08-25 16:34:26 -0400

    Newest version of APE redo.  Not completed.

--- runqchem.sh
+++ runqchem.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 . `dirname $0`/qcrc 
-echo qchem -nt $2 $3 $1 
-echo 1 = $1
-echo 2 = $2
-echo 3 = $3
+#echo qchem -nt $2 $3 $1 
+#echo 1 = $1
+#echo 2 = $2
+#echo 3 = $3
 qchem -nt $2 $3 $1
 
